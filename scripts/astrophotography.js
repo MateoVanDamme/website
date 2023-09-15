39bb350308ffb0ac99ca76b5b766fb13d1b8916d
Mateo Van Damme <mateovandamme1@gmail.com>
2023-09-15 23:42:33 +0200

    add more content to astrophotography page

--- scripts/astrophotography.js
+++ scripts/astrophotography.js
@@ -1,5 +1,6 @@
 import { addButtonClickListener } from './modules/button-module.js'
 
 addButtonClickListener('btnGitHubAstrophotography', 'https://github.com/MateoVanDamme');
-addButtonClickListener('btnInstagramMateo', 'https://www.instagram.com/mateovandamme/');
+addButtonClickListener('btnGitHubAstrophotography2', 'https://github.com/MateoVanDamme');
+addButtonClickListener('btnInstagramMateoAstro', 'https://www.instagram.com/mateovandamme_astro/');
 addButtonClickListener('btnFacebookMateo', 'https://www.facebook.com/mateo.vandamme.5');
