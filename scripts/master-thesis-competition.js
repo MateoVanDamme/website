87db020e41623e4691fe448406bfe503a3e0974f
Mateo Van Damme <mateovandamme1@gmail.com>
2023-11-28 21:04:08 +0100

    added competition page

---
+++ scripts/master-thesis-competition.js
@@ -0,0 +1,7 @@
+import { addButtonClickListener } from './modules/button-module.js'
+import { createNavbar } from './modules/navbar-builder.js'
+
+createNavbar("Master thesis competition");
+//Add buttons
+addButtonClickListener('btnFutureProef', 'https://www.eoswetenschap.eu/scriptieprijs2023');
+addButtonClickListener('btnEos2023', 'https://www.eoswetenschap.eu/scriptieprijs2023');
